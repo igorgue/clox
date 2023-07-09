7c5badc691ca20938df79a15d5857189e858a89a
Igor Guerrero <igorgue@protonmail.com>
2023-07-08 21:53:58 -0600

    feat(scan): add scanner

--- vm.c
+++ vm.c
@@ -1,6 +1,7 @@
 #include "vm.h"
 #include "chunk.h"
 #include "common.h"
+#include "compiler.h"
 #include "debug.h"
 #include <stdio.h>
 
@@ -64,8 +65,7 @@ static InterpretResult run() {
 #undef BINARY_OP
 }
 
-InterpretResult interpret(Chunk *chunk) {
-  vm.chunk = chunk;
-  vm.ip = vm.chunk->code;
-  return run();
+InterpretResult interpret(const char *source) {
+  compile(source);
+  return INTERPRET_OK;
 }
