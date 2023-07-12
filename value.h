7dbdf90716cdf93a7167850be5660f1e2ca4d8e7
Igor Guerrero <igorgue@protonmail.com>
2023-07-11 19:05:25 -0600

    More wip

--- value.h
+++ value.h
@@ -9,21 +9,30 @@ typedef enum {
   VAL_NUMBER,
 } ValueType;
 
-typedef double Value;
-
-typedef struct {
-  int capacity;
-  int count;
-  Value *values;
-} ValueArray;
-
 typedef struct {
   ValueType type;
   union {
     bool boolean;
     double number;
   } as;
-} ValueObject;
+} Value;
+
+#define IS_BOOL(value) ((value).type == VAL_BOOL)
+#define IS_NIL(VALUE) ((value).type == VAL_NIL)
+#define IS_NUMBER(value) ((value).type == VAL_NUMBER)
+
+#define AS_BOOL(value) ((value).as.boolean)
+#define AS_NUMBER(value) ((value).as.number)
+
+#define BOOL_VAL(value) ((Value){VAL_BOOL, {.boolean = value}})
+#define NIL_VAL ((Value){VAL_NIL, {.number = 0}})
+#define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
+
+typedef struct {
+  int capacity;
+  int count;
+  Value *values;
+} ValueArray;
 
 void initValueArray(ValueArray *array);
 void writeValueArray(ValueArray *array, Value value);
