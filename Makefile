c8fe20b042d40880003b44ed372808fe979c17b8
Igor Guerrero <igorgue@protonmail.com>
2023-08-21 22:04:00 -0600

    more wip

--- Makefile
+++ Makefile
@@ -1,27 +1,8 @@
-### If you wish to use extra libraries (math.h for instance),
-### add their flags here (-lm in our case) in the "LIBS" variable.
-
-LIBS = -lm
-
-###
-CFLAGS  = -std=c99
-CFLAGS += -g
-# CFLAGS += -Wall
-CFLAGS += -Wextra
-CFLAGS += -pedantic
-CFLAGS += -Werror
-CFLAGS += -Wmissing-declarations
-CFLAGS += -DUNITY_SUPPORT_64 -DUNITY_OUTPUT_COLOR
-
-ASANFLAGS  = -fno-common
-# ASANFLAGS += -fsanitize=address
-ASANFLAGS += -fno-omit-frame-pointer
-
 SRC = $(wildcard *.h) $(wildcard *.c)
 
 .PHONY: clox
 clox:
-	$(CC) $(CFLAGS) $(ASANFLAGS) $(LIBS) -o clox $(SRC)
+	$(CC) -o clox $(SRC)
 
 .PHONY: clean
 clean:
--- compiler.c
+++ compiler.c
@@ -31,7 +31,7 @@ typedef enum {
   PREC_PRIMARY
 } Precedence;
 
-typedef void (*ParseFn)();
+typedef void (*ParseFn)(bool canAssign);
 
 typedef struct {
   ParseFn prefix;
@@ -153,7 +153,11 @@ static void parsePrecedence(Precedence precedence) {
   while (precedence <= getRule(parser.current.type)->precedence) {
     advance();
     ParseFn infixRule = getRule(parser.previous.type)->infix;
-    infixRule();
+    infixRule(canAssign);
+  }
+
+  if (canAssign && match(TOKEN_EQUAL)) {
+    error("Invalid assignment target.");
   }
 }
 
@@ -170,7 +174,7 @@ static void defineVariable(uint8_t global) {
   emitBytes(OP_DEFINE_GLOBAL, global);
 }
 
-static void binary() {
+static void binary(bool canAssign) {
   TokenType operatorType = parser.previous.type;
   ParseRule *rule = getRule(operatorType);
   parsePrecedence((Precedence)(rule->precedence + 1));
@@ -211,7 +215,7 @@ static void binary() {
   }
 }
 
-static void literal() {
+static void literal(bool canAssign) {
   switch (parser.previous.type) {
   case TOKEN_FALSE:
     emitByte(OP_FALSE);
@@ -227,28 +231,30 @@ static void literal() {
   }
 }
 
-static void number() {
+static void number(bool canAssign) {
   double value = strtod(parser.previous.start, NULL);
   emitConstant(NUMBER_VAL(value));
 }
 
-static void string() {
+static void string(bool canAssign) {
   emitConstant(OBJ_VAL(
       copyString(parser.previous.start + 1, parser.previous.length - 2)));
 }
 
-static void namedVariable(Token name) {
+static void namedVariable(Token name, bool canAssign) {
   uint8_t arg = identifierConstant(&name);
 
-  if (match(TOKEN_EQUAL)) {
+  if (canAssign && match(TOKEN_EQUAL)) {
     expression();
     emitBytes(OP_SET_GLOBAL, arg);
   } else {
-    emitByte(OP_GET_GLOBAL);
+    emitBytes(OP_GET_GLOBAL, arg);
   }
 }
 
-static void variable() { namedVariable(parser.previous); }
+static void variable(bool canAssign) {
+  namedVariable(parser.previous, canAssign);
+}
 
 static void expression() { parsePrecedence(PREC_ASSIGNMENT); }
 
@@ -322,12 +328,12 @@ static void statement() {
   }
 }
 
-static void grouping() {
+static void grouping(bool canAssign) {
   expression();
   consume(TOKEN_RIGHT_PAREN, "Expect ')' after expression.");
 }
 
-static void unary() {
+static void unary(bool canAssign) {
   TokenType operatorType = parser.previous.type;
 
   // Compile the operand.
