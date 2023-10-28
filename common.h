4c1abb6b12394d187d8d1d1208a3251bb172336a
Igor Guerrero <igorgue@protonmail.com>
2023-10-27 20:00:07 -0600

    common.h checked

--- common.h
+++ common.h
@@ -4,7 +4,6 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
-#include <string.h>
 
 #define NAN_BOXING
 #define DEBUG_PRINT_CODE
@@ -15,3 +14,7 @@
 #define UINT8_COUNT (UINT8_MAX + 1)
 
 #endif
+#undef DEBUG_PRINT_CODE
+#undef DEBUG_TRACE_EXECUTION
+#undef DEBUG_STRESS_GC
+#undef DEBUG_LOG_GC
