3a5af53efd2126f11f5a8f39f3cf7f7ccda30c1e
Igor Guerrero <igorgue@protonmail.com>
2023-10-27 20:41:50 -0600

    object.c checked

--- memory.h
+++ memory.h
@@ -2,7 +2,7 @@
 #define clox_memory_h
 
 #include "common.h"
-#include "value.h"
+#include "object.h"
 
 #define ALLOCATE(type, count)                                                  \
   (type *)reallocate(NULL, 0, sizeof(type) * (count))
@@ -11,17 +11,17 @@
 
 #define GROW_CAPACITY(capacity) ((capacity) < 8 ? 8 : (capacity)*2)
 
-void *reallocate(void *pointer, size_t oldSize, size_t newSize);
-void markObject(Obj *object);
-void markValue(Value value);
-void collectGarbage();
-void freeObjects();
-
 #define GROW_ARRAY(type, pointer, oldCount, newCount)                          \
   (type *)reallocate(pointer, sizeof(type) * (oldCount),                       \
                      sizeof(type) * (newCount))
 
 #define FREE_ARRAY(type, pointer, oldCount)                                    \
   reallocate(pointer, sizeof(type) * (oldCount), 0)
+
+void *reallocate(void *pointer, size_t oldSize, size_t newSize);
+void markObject(Obj *object);
+void markValue(Value value);
+void collectGarbage();
+void freeObjects();
 
 #endif
--- object.c
+++ object.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "chunk.h"
 #include "memory.h"
 #include "object.h"
 #include "table.h"
@@ -19,23 +18,17 @@ static Obj *allocateObject(size_t size, ObjType type) {
   object->next = vm.objects;
   vm.objects = object;
 
-#ifdef DEBUG_LOG_GC
-  printf("%p allocate %ld for %d\n", (void *)object, size, type);
-#endif
-
   return object;
 }
-
 ObjBoundMethod *newBoundMethod(Value receiver, ObjClosure *method) {
   ObjBoundMethod *bound = ALLOCATE_OBJ(ObjBoundMethod, OBJ_BOUND_METHOD);
   bound->receiver = receiver;
   bound->method = method;
   return bound;
 }
-
 ObjClass *newClass(ObjString *name) {
   ObjClass *klass = ALLOCATE_OBJ(ObjClass, OBJ_CLASS);
-  klass->name = name;
+  klass->name = name; // [klass]
   initTable(&klass->methods);
   return klass;
 }
@@ -51,19 +44,6 @@ ObjClosure *newClosure(ObjFunction *function) {
   closure->upvalues = upvalues;
   closure->upvalueCount = function->upvalueCount;
   return closure;
-}
-
-static ObjString *allocateString(char *chars, int length, uint32_t hash) {
-  ObjString *string = ALLOCATE_OBJ(ObjString, OBJ_STRING);
-  string->length = length;
-  string->chars = chars;
-  string->hash = hash;
-
-  push(OBJ_VAL(string));
-  tableSet(&vm.strings, string, NIL_VAL);
-  pop();
-
-  return string;
 }
 
 ObjFunction *newFunction() {
@@ -88,10 +68,23 @@ ObjNative *newNative(NativeFn function) {
   return native;
 }
 
+static ObjString *allocateString(char *chars, int length, uint32_t hash) {
+  ObjString *string = ALLOCATE_OBJ(ObjString, OBJ_STRING);
+  string->length = length;
+  string->chars = chars;
+  string->hash = hash;
+
+  push(OBJ_VAL(string));
+  tableSet(&vm.strings, string, NIL_VAL);
+  pop();
+
+  return string;
+}
+
 static uint32_t hashString(const char *key, int length) {
   uint32_t hash = 2166136261u;
   for (int i = 0; i < length; i++) {
-    hash ^= key[i];
+    hash ^= (uint8_t)key[i];
     hash *= 16777619;
   }
   return hash;
@@ -148,14 +141,17 @@ void printObject(Value value) {
   case OBJ_CLOSURE:
     printFunction(AS_CLOSURE(value)->function);
     break;
+  case OBJ_FUNCTION:
+    printFunction(AS_FUNCTION(value));
+    break;
+  case OBJ_INSTANCE:
+    printf("%s instance", AS_INSTANCE(value)->klass->name->chars);
+    break;
   case OBJ_NATIVE:
     printf("<native fn>");
     break;
   case OBJ_STRING:
     printf("%s", AS_CSTRING(value));
-    break;
-  case OBJ_FUNCTION:
-    printFunction(AS_FUNCTION(value));
     break;
   case OBJ_UPVALUE:
     printf("upvalue");
